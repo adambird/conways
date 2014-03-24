5886c563ab2450ae75b4b7bca40f67e7802df29a
Adam Bird <adam.bird@gmail.com>
2014-03-24 08:41:59 +0000

    added delay to rendering

--- game_of_life.py
+++ game_of_life.py
@@ -1,8 +1,9 @@
 from eco_system import *
 from eco_system_renderer import *
+import time
 
-size = 20
-generations = 30
+size = 40
+generations = 300
 
 system = EcoSystem(size)
 
@@ -13,3 +14,4 @@ render_system(system)
 for generation in range(0, generations):
   system.advance_generation()
   render_system(system)
+  time.sleep(0.1)
